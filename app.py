d67b855a23e42a1a452a995d0d4ba41cd28db65d
brunosci <155730893+brunosci@users.noreply.github.com>
2024-01-09 15:43:12 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1125,9 +1125,8 @@ else:
     
       stock_data['Upper_BB'] = stock_data['MA'] + std_multiplier * stock_data['std']
       stock_data['Lower_BB'] = stock_data['MA'] - std_multiplier * stock_data['std']
-    
-      stock_data['Condition'] = (stock_data['Upper_BB']/stock_data['Lower_BB']) > (1 + (distance/100))
-
+      stock_data['Division'] =  stock_data['Upper_BB']/stock_data['Lower_BB']   
+      stock_data['Condition'] = stock_data['Division'].apply(lambda x: True if x > (1 + (distance / 100)) else False)
 
       in_trade = False
       entry_price = 0
